c66679c0e51d12b5c105989c3199bd55399085c8
FabBeyond <redcraft42.12@gmail.com>
2025-01-02 23:24:27 +0100

    Initial commit

---
+++ about.md
@@ -0,0 +1,4 @@
+# EditorSongFetch
+
+This adds a button to the menu where you can edit, play, publish the level.
+The button downloads the song used in the level, so you dont have to go into the editor to download it.
---
+++ src/main.cpp
@@ -0,0 +1,44 @@
+#include <Geode/Geode.hpp>
+
+using namespace geode::prelude;
+
+#include <Geode/binding/MusicDownloadManager.hpp>
+#include <Geode/modify/EditLevelLayer.hpp>
+class $modify(MyEditLevelLayer, EditLevelLayer) {
+    bool init(GJGameLevel* p0) {
+        if (!EditLevelLayer::init(p0)) return false;
+
+        auto levelactionmenu = this->getChildByID("level-actions-menu");
+
+        auto downloadSongButton = CCMenuItemSpriteExtra::create(
+            CCSprite::createWithSpriteFrameName("GJ_downloadBtn_001.png"),
+            this,
+            menu_selector(MyEditLevelLayer::downloadSongButtonPressed)
+        );
+        downloadSongButton->setUserObject(p0);
+
+        levelactionmenu->addChild(downloadSongButton);
+        downloadSongButton->setID("download-song-button");
+
+        levelactionmenu->updateLayout();
+
+        return true;
+    }
+
+
+    void downloadSongButtonPressed(CCObject*) {
+
+        auto levelActionMenu = this->getChildByID("level-actions-menu");
+            
+        auto downloadSongButton = levelActionMenu->getChildByID("download-song-button");
+
+        auto p0 = dynamic_cast<GJGameLevel*>(downloadSongButton->getUserObject());
+
+        int songId;
+        songId = p0->m_songID;
+        std::string sondIDstring = std::to_string(songId);
+        log::debug("Song ID: {}", songId);
+
+        MusicDownloadManager::sharedState()->downloadSong(songId);
+    }
+};
